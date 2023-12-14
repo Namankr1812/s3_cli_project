fe208c95df6ed2790d3d029183882177d2fe3138
Naman Kumar <133354548+Namankr1812@users.noreply.github.com>
2023-12-14 22:22:49 +0530

    Add files via upload

--- s3_cli.py
+++ s3_cli.py
@@ -3,9 +3,15 @@ import boto3
 import os
 import time
 import math
+import datetime
+import uuid
+import logging
+
 
 from botocore.exceptions import NoCredentialsError
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from click import prompt
+
 
 
 session = boto3.Session()
@@ -20,11 +26,19 @@ def cli():
 session = boto3.Session()
 s3 = session.resource('s3')
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize the bucket variable
+bucket = None
+
 @cli.command()
 def user():
     """
     Main function to interact with the S3 CLI.
     """
+    global bucket  # Declare bucket as a global variable
+
     click.echo("Welcome to the S3 CLI!")
 
     # Prompt the user to set up an ID and password
@@ -36,6 +50,8 @@ def user():
     # Prompt the user for the S3 bucket name
     bucket = click.prompt("Enter the S3 bucket name")
 
+    # ...
+
     while True:
         click.echo("\nWhat would you like to do?")
         click.echo("1. Create a new folder in S3")
@@ -43,76 +59,103 @@ def user():
         click.echo("3. List the contents of a folder in S3")
         click.echo("4. Delete files in S3")
         click.echo("5. Download a file from S3")
-        click.echo("6. Exit")
+        click.echo("6. View S3 bucket activity logs")
+        click.echo("7. Exit")
 
         choice = click.prompt("Enter your choice")
 
         if choice == "1":
             folder_name = click.prompt("Enter the folder name")
-            create_folder(folder_name)
+            create_folder(folder_name, bucket)
+            logging.info(f"Created folder: {folder_name}")
         elif choice == "2":
             folder_path = click.prompt("Enter the folder path")
             upload_folder(folder_path, bucket)
+            logging.info(f"Uploaded files from {folder_path} to S3 bucket {bucket}")
         elif choice == "3":
-            list_folder_contents(bucket)  # Pass 'bucket' to the function
+            list_folder_contents(bucket)
+            logging.info(f"Listed contents of S3 bucket {bucket}")
         elif choice == "4":
             files_to_delete = click.prompt("Enter the file names to delete (separated by space)").split()
             delete_files(bucket, files_to_delete)
+            logging.info(f"Deleted files {files_to_delete} from S3 bucket {bucket}")
         elif choice == "5":
             download_from_s3(bucket)
+            logging.info(f"Downloaded a file from S3 bucket {bucket}")
         elif choice == "6":
+            view_s3_bucket_logs(bucket)
+        elif choice == "7":
             break
         else:
             click.echo("Invalid choice. Please try again.")
 
+def view_s3_bucket_logs(bucket_name):
+    try:
+        # Create an S3 resource using the provided bucket name
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+
+        # Iterate over the bucket's objects and log their key and size
+        for obj in bucket.objects.all():
+            click.echo(obj)
+            click.echo(f"Object: {obj.key}, Size: {obj.size} bytes")
+
+        click.echo(f"S3 bucket activity logs for '{bucket_name}':")
+    except Exception as e:
+        click.echo(f"Error retrieving S3 bucket activity logs: {str(e)}")
 import uuid
 
-def create_folder(folder_name, region="eu-north-1"):
+def create_folder(folder_name, user_bucket, region="eu-north-1"):
     """
-    Create a new folder in S3 with a unique identifier.
+    Create a new folder (object key prefix) within the user-specified bucket.
     """
     try:
-        unique_id = str(uuid.uuid4())[:8]  # Generate a unique identifier (e.g., first 8 characters of a UUID)
-        unique_folder_name = f"{folder_name}-{unique_id}"
+        unique_id = str(uuid.uuid4())[:8]
+        unique_folder_name = f"{folder_name}-{unique_id}/"  # Add a trailing slash for visual hierarchy
 
         s3 = session.client('s3', region_name=region)
-        s3.create_bucket(Bucket=unique_folder_name, CreateBucketConfiguration={'LocationConstraint': region})
+        s3.put_object(Bucket=user_bucket, Key=unique_folder_name)
 
-        click.echo(f"Folder {unique_folder_name} created successfully.")
+        click.echo(f"Folder {unique_folder_name} created successfully within '{user_bucket}'.")
     except NoCredentialsError:
         click.echo("Credentials not available. Please set up your AWS credentials.")
     except Exception as e:
         click.echo(f"An error occurred: {str(e)}")
+        logging.error(f"Error creating folder: {str(e)}")
+
+# Assuming you have the necessary imports and session setup here...
+
+def calculate_number_of_parts(file_size, part_size):
+    """
+    Calculate the number of parts needed for a multipart upload.
+    """
+    return (file_size + part_size - 1) // part_size
 
 def upload_part(args):
     """
     Upload a part of the file to S3.
     """
-    try:
-        part_number, upload_id, file_path, bucket, file_name, part_size = args
-        s3 = session.client('s3')
+    part_number, upload_id, file_path, bucket, file_name, part_size = args
+    s3 = session.client('s3')
 
-        with open(file_path, 'rb') as file:
-            file.seek(part_size * (part_number - 1))
-            data = file.read(part_size)
+    with open(file_path, 'rb') as file:
+        file.seek(part_size * (part_number - 1))
+        data = file.read(part_size)
 
-            response = s3.upload_part(
-                Body=data,
-                Bucket=bucket,
-                Key=file_name,
-                UploadId=upload_id,
-                PartNumber=part_number
-            )
+        response = s3.upload_part(
+            Body=data,
+            Bucket=bucket,
+            Key=file_name,
+            UploadId=upload_id,
+            PartNumber=part_number
+        )
 
-            etag = response["ETag"]
-            return {"PartNumber": part_number, "ETag": etag}
+        etag = response["ETag"]
+        return {"PartNumber": part_number, "ETag": etag}
 
-    except Exception as e:
-        return {"PartNumber": part_number, "Error": str(e)}
-
-def upload_folder(folder_path, bucket):
+def upload_folder(folder_path, bucket, target_part_size=None):
     """
-    Upload files from a folder to S3.
+    Upload files from a folder to S3 with parallel processing.
     """
     try:
         # Upload files in the folder
@@ -123,6 +166,35 @@ def upload_folder(folder_path, bucket):
         file_paths = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if
                       os.path.isfile(os.path.join(folder_path, file))]
 
+        # Prompt the user for the number of parts only once
+        if target_part_size is None:
+            total_file_size = sum(os.path.getsize(file) for file in file_paths)
+
+            # Calculate the default part size based on file size
+            if total_file_size > 100 * 1024 * 1024:  # Large file
+                target_part_size = 100 * 1024 * 1024  # Set a maximum part size for large files
+            else:  # Small file
+                target_part_size = 5 * 1024 * 1024  # Set a default part size for small files
+
+            default_num_parts = calculate_number_of_parts(total_file_size, target_part_size)
+
+            # Prompt the user for part size
+            target_part_size = click.prompt("Enter part size (in bytes)", default=target_part_size, type=int)
+
+            # Check if the user exceeds the maximum part size
+            if target_part_size > 100 * 1024 * 1024:
+                click.echo(f"Warning: Part size cannot exceed 100 MB. Using the default part size.")
+                target_part_size = 100 * 1024 * 1024
+
+            # Check if the user enters a custom part size less than the minimum allowed
+            min_part_size = 5 * 1024 * 1024  # Minimum allowed part size
+            if target_part_size < min_part_size:
+                click.echo(f"Warning: Part size cannot be less than 5 MB. Using the minimum part size.")
+                target_part_size = min_part_size
+
+            # Calculate the number of parts based on the user's input
+            num_parts_prompt = click.prompt(f"Enter the number of parts for all files (default is {default_num_parts})", default=default_num_parts, type=int, show_default=default_num_parts)
+
         for file_path in file_paths:
             file_name = os.path.basename(file_path)
             response = s3.create_multipart_upload(Bucket=bucket, Key=file_name)
@@ -130,11 +202,11 @@ def upload_folder(folder_path, bucket):
 
             # Adjust part size based on file size
             file_size = os.path.getsize(file_path)
-            part_size = 100 * 1024 * 1024 if file_size > 100 * 1024 * 1024 else 5 * 1024 * 1024  # 100MB for large files, 5MB for small files
+            part_size = target_part_size
+            num_parts = min(5, file_size // part_size + 1)
 
             # Use ThreadPoolExecutor for parallel uploads
             with ThreadPoolExecutor(max_workers=5) as executor:
-                num_parts = min(5, file_size // part_size + 1)
                 args_list = [(part_number, upload_id, file_path, bucket, file_name, part_size)
                              for part_number in range(1, num_parts + 1)]
 
@@ -272,7 +344,6 @@ def download_from_s3(bucket):
         click.echo(f"An error occurred: {str(e)}")
 
 #------------------------------------------------------watch-------------------------------------------------------------------
-
 def get_file_state(directory, recursive):
     file_state = set()
     for root, dirs, files in os.walk(directory):
